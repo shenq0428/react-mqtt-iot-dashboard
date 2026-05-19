73fc7a5fc55336b05c3b7e1bb6956a90ac759e83
shenq0428 <wzshenq0428@gmail.com>
2026-05-19 13:51:00 +0800

    added disconnect and connected frontend count in backend

--- backend/gateway/mqttSocketBridge.js
+++ backend/gateway/mqttSocketBridge.js
@@ -1,6 +1,21 @@
 const mqtt = require("mqtt")
 
 function startMQTTBridge(io) {
+  // WebSocket frontend connected
+   let connectedFrontendCount =0
+  io.on("connection", (socket) => {
+    
+    connectedFrontendCount++
+    console.log("Frontend connected")
+    console.log("Socket ID:", socket.id)
+    console.log("Online Frontends:", connectedFrontendCount)
+    // Frontend disconnected
+    socket.on("disconnect", () => {
+      connectedFrontendCount--
+      console.log("Frontend disconnected")
+      console.log("Online Frontends:", connectedFrontendCount)
+    })
+  })
   // MQTT Connection
   const client = mqtt.connect(process.env.MQTT_URL, {
     username: process.env.MQTT_USERNAME,
