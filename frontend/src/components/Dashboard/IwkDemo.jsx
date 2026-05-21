86470492e2d8846c0b4cd443a05745882f978659
shenq0428 <wzshenq0428@gmail.com>
2026-05-21 17:00:20 +0800

    done iwk demo dashboard panel dislay but not yet done logic display part

--- frontend/src/components/Dashboard/IwkDemo.jsx
+++ frontend/src/components/Dashboard/IwkDemo.jsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react"
 import socket from "../../services/socketClient"
-import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip
-} from "recharts"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts"
+//icon import 
+import { Wifi, Activity, Database, Clock3 } from "lucide-react"
+
 
 function IwkDemo() {
     const equipmentColors = {
@@ -19,9 +15,13 @@ function IwkDemo() {
     const [graphData, setGraphData] = useState([])
 
     // SELECTED EQUIPMENTS
-    const [selectedEquipments, setSelectedEquipments] = useState(["blower_1"])
+    const [selectedEquipments, setSelectedEquipments] = useState(["blower_1", "blower_2", "blower_3"])
 
     const [refreshInterval, setRefreshInterval] = useState(0)
+
+    //record message in websocket
+    const [messageCount, setMessageCount] = useState(0)
+
     // HANDLE CHECKBOX CLICK
     function handleEquipmentChange(equipment) {
         setSelectedEquipments((prev) => {
@@ -88,8 +88,9 @@ function IwkDemo() {
                 //  step 4:SORT TIME
                 groupedData.sort(
                     (a, b) =>
-                        new Date(`1970/01/01 ${a.time}`) -
-                        new Date(`1970/01/01 ${b.time}`)
+                        //new Date(`1970/01/01 ${a.time}`) -
+                        //new Date(`1970/01/01 ${b.time}`)
+                        a.timestamp - b.timestamp
                 )
                 //step 5
                 setGraphData(groupedData)
@@ -114,6 +115,8 @@ function IwkDemo() {
             // REALTIME MQTT WEBSOCKET
             socket.on("mqtt-message", (data) => {
 
+                setMessageCount((prev) => {
+                })
                 const roundedTime =
 
                     Math.floor(Date.now() / 1000) * 1000
@@ -182,11 +185,11 @@ function IwkDemo() {
 
             clearInterval(intervalId)
         }
-    }, [selectedEquipments,refreshInterval])
+    }, [selectedEquipments, refreshInterval])
 
     return (
 
-        <>
+        <div>
             {/* HEADER */}
             <div className="Iwk_header">
                 <h1>DEMO_IWK_260325</h1>
@@ -194,82 +197,158 @@ function IwkDemo() {
                     Graph Points: {graphData.length}
                 </h3>
             </div>
+            {/* DASHBOARD CARDS */}
+            <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "20px", marginBottom: "20px" }}
+            >
+                <div style={{ background: "#111827", borderRadius: "16px", padding: "20px", boxShadow: `0 0 15px rgba(0,255,255,0.18),0 0 30px rgba(0,255,255,0.10),0 0 60px rgba(0,255,255,0.05)` }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                        <div>
+                            <h3 style={{ color: "#00FFFF", marginBottom: 0 }}
+                            >MQTT Status</h3>
+                            <h1 style={{ color: "#4ADE80", marginBottom: "30px" }}
+                            > Connected</h1>
+                        </div>
+                        <Wifi size={42} color="#3B82F6" opacity={0.4} />
+                    </div>
+                </div>
+                <div style={{ background: "#111827", borderRadius: "16px", padding: "20px", boxShadow: `0 0 15px rgba(0,255,255,0.18),0 0 30px rgba(0,255,255,0.10),0 0 60px rgba(0,255,255,0.05)` }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                        <div>
+                            <h3 style={{ color: "#00FFFF", marginBottom: 0 }}
+                            >ACTIVE EQUIPMENT</h3>
+                            <h1 style={{ color: "yellow", marginBottom: "30px" }}
+                            >{selectedEquipments.length}</h1>
+                        </div>     <Activity size={42} color="#3B82F6" opacity={0.4} />
+                    </div>
+                </div>
+                <div style={{ background: "#111827", borderRadius: "16px", padding: "20px", boxShadow: `0 0 15px rgba(0,255,255,0.18),0 0 30px rgba(0,255,255,0.10),0 0 60px rgba(0,255,255,0.05)` }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                        <div>
+                            <h3 style={{ color: "#00FFFF", marginBottom: 0 }}
+                            >LIVE MESSAGES</h3>
+                            <h1 style={{ color: "yellow", marginBottom: "30px" }}
+                            >{messageCount}</h1>
+                        </div>    <Database size={42} color="#3B82F6" opacity={0.4} />
+                    </div></div>
+                <div style={{ background: "#111827", borderRadius: "16px", padding: "20px", boxShadow: `0 0 15px rgba(0,255,255,0.18),0 0 30px rgba(0,255,255,0.10),0 0 60px rgba(0,255,255,0.05)` }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
+                        <div>
+                            <h3 style={{ color: "#00FFFF", marginBottom: "0px" }}
+                            >SYSTEM UPTIME</h3>
+                            <h1 style={{ color: "yellow", marginBottom: "30px" }}
+                            > 0day 7h</h1>
+                        </div>
+                        <Clock3 size={42} color="#3B82F6" opacity={0.4} />
+                    </div></div>
+            </div>
+            {/* GRAPH */}
+            <div
+                style={{
+                    background: "#0b1220",
 
-            {/* TELEMETRY CHARTS */}
-            <div>
+                    padding: "20px",
 
-                <LineChart
-                    width={1000}
-                    height={300}
-                    data={graphData}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="time" />
-                    <YAxis />
-                    <Tooltip />
-                    {
-                        selectedEquipments.map((equipment) => (
-                            <Line
-                                key={equipment}
-                                type="monotone"
-                                dataKey={equipment}
-                                strokeWidth={2}
-                                stroke={equipmentColors[equipment]}
-                                dot={false}
-                                isAnimationActive={false}
-                                connectNulls={true}
-                            />
-                        ))
-                    }
+                    borderRadius: "12px",
 
-                </LineChart>
+                    width: "100%"
+                }}
+            >
+                {/* TELEMETRY CHARTS */}
+                <div>
+
+                    <LineChart
+                        width={1400}
+                        height={500}
+                        data={graphData}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="time" />
+                        <YAxis />
+                        <Tooltip />
+                        {
+                            selectedEquipments.map((equipment) => (
+                                <Line
+                                    key={equipment}
+                                    type="natural"
+                                    dataKey={equipment}
+                                    strokeWidth={2}
+                                    stroke={equipmentColors[equipment]}
+                                    dot={false}
+                                    isAnimationActive={false}
+                                    connectNulls={true}
+                                />
+                            ))
+                        }
+
+                    </LineChart>
+                </div>
             </div>
             <div
                 style={{
                     display: "flex",
-                    alignItems: "center",
-                    gap: "12px",
+                    justifyContent: "flex-end",
+                    marginBottom: "20px"
+                }}
+            >
+                <div className="refresh_button"
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "12px",
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                        padding: "12px 16px",
+                        background: "#111827",
+                        borderRadius: "10px",
+                        width: "fit-content"
+                    }}
+                >
+
+                    <h4 style={{ margin: 0, color: "#E5E7EB" }}>
+                        REFRESH MODE
+                    </h4>
+
+                    <select
+                        value={refreshInterval}
+
+                        onChange={(e) =>
+                            setRefreshInterval(
+                                Number(e.target.value)
+                            )
+                        }
+                        style={{
+                            padding: "6px 10px", borderRadius: "6px", border: "none", background: "#1F2937", color: "white", cursor: "pointer"
+                        }}
+                    >
+                        <option value={0}>
+                            AUTO</option>
+                        <option value={3000}>
+                            3 Seconds</option>
+                        <option value={5000}>
+                            5 Seconds</option>
+                        <option value={10000}>
+                            10 Seconds </option>
+                        <option value={30000}>
+                            30 Seconds</option>
+                    </select>
+
+                </div>
+            </div>
+            {/* EQUIPMENT FILTER */}
+            <div
+                style={{
                     marginTop: "20px",
-                    marginBottom: "20px",
-                    padding: "12px 16px",
-                    background: "#111827",
-                    borderRadius: "10px",
-                    width: "fit-content"
+
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20px"
                 }}
             >
 
-                <h4 style={{ margin: 0, color: "#E5E7EB" }}>
-                    Auto Refresh
-                </h4>
-
-                <select
-                    value={refreshInterval}
-
-                    onChange={(e) =>
-                        setRefreshInterval(
-                            Number(e.target.value)
-                        )
-                    }
-                    style={{
-                        padding: "6px 10px", borderRadius: "6px", border: "none", background: "#1F2937", color: "white", cursor: "pointer"
-                    }}
-                >
-                    <option value={0}>
-                        AUTO</option>
-                    <option value={3000}>
-                        3 Seconds</option>
-                    <option value={5000}>
-                        5 Seconds</option>
-                    <option value={10000}>
-                        10 Seconds </option>
-                    <option value={30000}>
-                        30 Seconds</option>
-                </select>
-
-            </div>
-            {/* EQUIPMENT FILTER */}
-            <div>
                 <label>
+                    <button onClick={() => setSelectedEquipments(["blower_1", "blower_2", "blower_3"])}>
+                        SELECT ALL
+                    </button>
                     <input
                         type="checkbox"
                         checked={
@@ -314,7 +393,7 @@ function IwkDemo() {
                     Blower 3
                 </label>
             </div>
-        </>
+        </div>
     )
 }
 
